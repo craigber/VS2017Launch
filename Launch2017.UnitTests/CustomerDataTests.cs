d92e782f62bffe7760b37061245c0bfeda1a5359
craigber <craig@craigberntson.com>
2017-03-01 15:07:01 -0700

    Add code and unit tests for Fakes

---
+++ Launch2017.UnitTests/CustomerDataTests.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Launch2017;
+//using Launch2017.Fakes;
+
+//namespace Launch2017.UnitTests
+//{
+//    [TestClass]
+//    public class CustomerDataTests
+//    {
+//        [TestMethod]
+//        public void SaveShouldUpdateTheRepository()
+//        {
+//            // Arrange
+
+//            // Create a variable to stash the actual value passed to the repository. 
+//            var savedCustomer = default(Customer); // null
+
+//            // Create a new stub repository
+//            var repository = new StubICustomerRepository
+//            {
+//                // this is a closure
+//                SaveOrUpdateCustomer = customer => savedCustomer = customer
+//            };
+
+//            // Create a customer object to pass to the view model
+//            var actualCustomer = new Customer { Id = 1, Name = "Nerry Jixon" };
+
+//            // Create the view model we will test
+//            var viewModel = new CustomerViewModel(actualCustomer, repository);
+
+//            // Act
+
+//            // Call the save method on the view model
+//            viewModel.Save();
+
+//            // Assert
+
+//            // Verify the customer object is the same one we gave to the view model
+//            Assert.IsNotNull(savedCustomer);
+//            Assert.AreEqual(1, savedCustomer.Id);
+//            Assert.AreEqual("Nerry Jixon", savedCustomer.Name);
+
+//        }
+//    }
+//}
---
+++ Launch2017/CustomerData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launch2017
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public interface ICustomerRepository
+    {
+        Customer GetById(int Id);
+        IEnumerable<Customer> GetAll();
+        Customer SaveOrUpdate(Customer customer);
+        void Delete(Customer customer);
+    }
+
+    public class CustomerViewModel
+    {
+        private Customer customer;
+        private readonly ICustomerRepository repository;
+
+        public CustomerViewModel(Customer customer, ICustomerRepository repository)
+        {
+            this.customer = customer;
+            this.repository = repository;
+        }
+
+        public string Name
+        {
+            get { return customer.Name; }
+            set
+            {
+                customer.Name = value;
+                // do something else
+            }
+        }
+
+        public void Save()
+        {
+            customer = repository.SaveOrUpdate(customer);
+        }
+    }
+}
